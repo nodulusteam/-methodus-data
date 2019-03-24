2433af46e8a1d6c1705ba278af61666f32e4f408
Roi ben haim (rb136m) <rb136m@att.com>
2019-03-24 19:40:20 +0200

    [SecurityContext] added context to event emitter

--- lib/emitter.ts
+++ lib/emitter.ts
@@ -2,8 +2,8 @@ import { EventEmitter } from 'events';
 import { DataChangeEvent } from './changes';
 
 export class DataEmitter extends EventEmitter {
-    changes(topic, changeData: DataChangeEvent) {
-        this.emit(topic, JSON.stringify(changeData));
+    changes(topic, changeData: DataChangeEvent, securityContext?: any) {
+        this.emit(topic, changeData, securityContext);
     }
 }
 
